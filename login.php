f6c635ed1150c5c0bdfbc6f95d8fa3ca8109ba40
Parvathy36 <parvathysuresh36@gmail.com>
2024-02-19 11:14:56 +0530

    done

--- login.php
+++ login.php
@@ -64,7 +64,7 @@ if (isset($_POST['sub'])) {
     <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
     <!-- Title  -->
-    <title>Karl - Fashion Ecommerce Template | Home</title>
+    <title> Aura</title>
 
     <!-- Favicon  -->
     <link rel="icon" href="img/core-img/favicon.ico">
--- register.php
+++ register.php
@@ -53,7 +53,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
     <!-- Title  -->
-    <title>Karl - Fashion Ecommerce Template | Home</title>
+    <title>Aura</title>
 
     <!-- Favicon  -->
     <link rel="icon" href="img/core-img/favicon.ico">
